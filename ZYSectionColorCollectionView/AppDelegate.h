a48f9029ef9b6c8625525bdaf60c39f12ba9678f
zy <zhangyue@renwohua.com>
2017-08-04 15:52:22 +0800

    Initial Commit

---
+++ ZYSectionColorCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYSectionColorCollectionView
+//
+//  Created by zy on 2017/8/4.
+//  Copyright © 2017年 zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
